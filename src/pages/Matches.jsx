816e1aee3dfe70b4144f3e0a18f85696b8614f37
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-06-08 00:10:53 +0200

    add css rules to root

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getMatches } from "../services/matchesService"
+import SidebarNav from "../components/Sidebar"
 
 export default function Matches(){
     const [matches, setMatches] = useState([])
@@ -19,6 +20,7 @@ export default function Matches(){
     
     return(
         <>
+            <SidebarNav/>
             <div>
                 {matches.map(match => (
                     <div key={match.id} className="flex flex-row">
