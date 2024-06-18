c45f65053f0cc50894ae849ead07168b51ceab36
unknown <stopkakrzysztof1@gmail.com>
2024-06-18 16:52:27 +0200

    Add new button to home page

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -3,13 +3,12 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useContext } from "react";
 import { motion } from "framer-motion";
 import { UserContext } from "../context/UserContext";
-import { IoHomeOutline } from "react-icons/io5";
+import { IoHomeOutline, IoLogOutOutline } from "react-icons/io5";
 import { MdOutlineScoreboard } from "react-icons/md";
 import { LuTrophy } from "react-icons/lu";
-import { IoLogOutOutline } from "react-icons/io5";
 
 export default function BottomMenu() {
-  const {logout} = useContext(UserContext);
+  const { logout } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -18,7 +17,7 @@ export default function BottomMenu() {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 px-6 bg-yellow shadow-lg rounded-t-2xl py-2">
+    <div className="fixed bottom-0 left-0 right-0 px-6 bg-yellow shadow-lg rounded-t-2xl py-4">
       <div className="flex justify-around">
         <MenuItem to="/" icon={IoHomeOutline} text={'Home'} />
         <MenuItem to="/matches" icon={MdOutlineScoreboard} text={'Mecze'} />
@@ -39,16 +38,16 @@ function MenuItem({ to, icon: Icon, text, onClick }) {
           whileHover={{ scale: 1.1 }}
         >
           <motion.div
-            className="icon-wrapper relative"
+            className="relative flex justify-center items-center h-7.5"
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ type: "spring", stiffness: 300 }}
           >
-            <div className={`icon-container ${isActive ? 'active' : ''}`}>
-              <Icon className='w-7 h-7 relative z-10' />
+            <div className={`absolute w-10 h-10 flex justify-center items-center ${isActive ? 'bg-blue text-yellow rounded-full p-1.5 transform translate-y-[-15px] shadow-md' : 'text-darkblue'} transition duration-300`}>
+              <Icon className="w-7 h-7 relative z-10" />
               {isActive && (
                 <motion.div
-                  className="absolute inset-0 bg-blue-500 rounded-full z-0"
+                  className="absolute inset-0 bg-blue rounded-full z-0"
                   initial={{ scale: 0 }}
                   animate={{ scale: 1 }}
                   transition={{ type: "spring", stiffness: 300 }}
@@ -56,7 +55,7 @@ function MenuItem({ to, icon: Icon, text, onClick }) {
               )}
             </div>
           </motion.div>
-          <p className={`menu-text ${isActive ? 'text-blue' : 'text-darkblue'}`}>{text}</p>
+          <p className={`mt-4 text-xs ${isActive ? 'text-blue' : 'text-darkblue'} transition duration-300`}>{text}</p>
         </motion.div>
       )}
     </NavLink>
@@ -71,16 +70,16 @@ function LogoutButton({ onClick, icon: Icon, text }) {
       whileHover={{ scale: 1.1 }}
     >
       <motion.div
-        className="icon-wrapper"
+        className="relative flex justify-center items-center h-7.5"
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ type: "spring", stiffness: 300 }}
       >
-        <div className="icon-container relative">
-          <Icon className='w-7 h-7 relative z-10' />
+        <div className="absolute w-10 h-10 flex justify-center items-center transition duration-300">
+          <Icon className="w-7 h-7 relative z-10" />
         </div>
       </motion.div>
-      <p className="menu-text text-darkblue">{text}</p>
+      <p className="mt-4 text-xs text-blue transition duration-300">{text}</p>
     </motion.button>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import MobileNav from "../components/MobileNav"
 import logoHorizontal from "../img/icons/eurobet24_logo_horizontal.svg"
 import userAvatar from "../img/avatars/avatar_1.png"
 import { FaArrowRight } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const { user } = useContext(UserContext);
@@ -29,10 +30,6 @@ export default function Home() {
     };
   }, []);
 
-  const handleClick = (() => {
-
-  })
-
   return (
     <>
       {/* <SidebarNav /> */}
@@ -55,10 +52,18 @@ export default function Home() {
         </div>
         <div className='mt-5'>
           <h1 className='font-bold text-xl text-white'>Menu</h1>
-          <div className='flex flex-row items-center justify-between bg-yellow px-4 py-3 mt-5 rounded-xl' onClick={handleClick}>
+          <Link to="">
+          <div className='flex flex-row items-center justify-between border-2 border-white text-white px-4 py-3 mt-5 rounded-xl font-bold hover:bg-yellow hover:text-blue hover:border-yellow'>
+            <p>Dzisiejsze mecze</p>
+            <FaArrowRight />
+          </div>
+          </Link>
+          <Link to="/PreviousMatches">
+          <div className='flex flex-row items-center justify-between border-2 border-white text-white px-4 py-3 mt-5 rounded-xl font-bold hover:bg-yellow hover:text-blue hover:border-yellow'>
             <p>Poprzednie mecze</p>
             <FaArrowRight />
           </div>
+          </Link>
         </div>
       </div>
     </>
